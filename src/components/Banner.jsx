da39c378cf830020d34861858716612033b3b030
Rafiul Islam Jisan <rafiuljisan2005@gmail.com>
2026-03-05 01:53:14 +0600

    added some animation on banner section

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 import bannerImg from '../assets/hero.jpg'
 import { Link } from 'react-router';
 
@@ -11,13 +12,25 @@ const Banner = () => {
                     className='absolute inset-0 h-full w-full object-cover' />
                 <div className="absolute inset-0 bg-linear-to-b from-black/20 to-black/80 "></div>
                 <div className="absolute max-w-2xl w-full text-center">
-                    <h1 className="text-4xl lg:text-7xl font-bold text-white mb-4">
+                    <motion.h1
+                        initial={{ opacity: 0, y: 100 }}
+                        animate={{ opacity: 1, y: 0 }}
+                        transition={{ duration: .5, ease: 'easeIn', delay: .25 }}
+                        className="text-4xl lg:text-7xl font-bold text-white mb-4">
                         Save Lives, Share Blood
-                    </h1>
-                    <p className="text-lg text-gray-100 mb-8">
+                    </motion.h1>
+                    <motion.p
+                        initial={{ opacity: 0, y: 100 }}
+                        animate={{ opacity: 1, y: 0 }}
+                        transition={{ duration: .5, ease: 'easeIn', delay: .5 }}
+                        className="text-lg text-gray-100 mb-8">
                         Be a hero in your community. Donate blood and help those in need.
-                    </p>
-                    <div className="flex flex-col sm:flex-row gap-4 justify-center">
+                    </motion.p>
+                    <motion.div
+                        initial={{ opacity: 0, y: 50 }}
+                        animate={{ opacity: 1, y: 0 }}
+                        transition={{ duration: .5, ease: 'easeIn', delay: .75 }}
+                        className="flex flex-col sm:flex-row gap-4 justify-center">
                         <Link
                             to={'/register'}
                             className="max-w-md mx-auto sm:mx-0 bg-red-600 hover:bg-red-700 text-white font-semibold py-3 px-8 rounded-lg transition transform hover:scale-105 duration-200"
@@ -30,7 +43,7 @@ const Banner = () => {
                         >
                             Search Donors
                         </Link>
-                    </div>
+                    </motion.div>
                 </div>
             </div>
         </div>
